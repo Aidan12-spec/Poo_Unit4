25957e327d2cd924f46b15267035e99e113e2bed
Aidan12-spec <aidanflores43@gmail.com>
2025-08-03 22:14:23 -0500

    Fecha.java

---
+++ src/model/Fecha.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Fecha {
+    private String dia;
+
+    public Fecha(String dia) { this.dia = dia; }
+    public String getDia() { return dia; }
+    public void setDia(String dia) { this.dia = dia; }
+}
